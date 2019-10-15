a166b2d3f12c5d232ac230d6f5d19506b49fae6f
连浩丞 <RhythmLian@outlook.com>
2019-10-15 11:25:08 +0800

    update

--- main.c
+++ main.c
@@ -10,23 +10,23 @@
 
 const int MAX_ITER=1000;
 pthread_mutex_t mutex;
-unsigned long in_cir = 0, total = 0;
+unsigned in_cir = 0, total = 0;
 
-int is_in_circle(double x, double y){
-    return 1.0 - x*x - y*y>=0;
+int is_in_circle(double x, double y) {
+    return 1.0 - x * x - y * y >= 0;
 }
 
-double get_val(){
-    double val = rand()%1000/1000.0;
-    return rand()&1?-val:val;
+double get_val() {
+    double val = rand() / (RAND_MAX * 1.0);
+    return rand() & 1 ? -val : val;
 }
 
-void *build_montecario(void*args){
-    for(int i =0;i<MAX_ITER;++i){
-        double x=get_val(), y=get_val();
+void *build_montecario(void*args) {
+    for (int i = 0; i < MAX_ITER; ++i) {
+        double x = get_val(), y = get_val();
         pthread_mutex_lock(&mutex);
         ++total;
-        in_cir+=is_in_circle(x,y)?1:0;
+        in_cir += is_in_circle(x, y) ? 1 : 0;
         pthread_mutex_unlock(&mutex);
     }
     return NULL;
@@ -40,10 +40,7 @@ int main(void) {
         pthread_join(pthread_id, NULL);
     }
     sleep(1);
-    double PI = in_cir * 1.0 / total * 4;
-    printf("in cricle:%lu total:%lu PI = %lf\n", in_cir, total, PI);
-    FILE*f = fopen("result.txt","a");
-    fprintf(f, "%lf\n", PI);
-    fclose(f);
+    double PI = 4.0 * in_cir / total;
+    printf("in cricle:%u total:%u PI = %lf\n", in_cir, total, PI);
     return 0;
 }
