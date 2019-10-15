b4029e8977f8dd1b2cf016c680e099217ca07966
连浩丞 <RhythmLian@outlook.com>
2019-10-15 14:42:49 +0800

    update

--- experiment/CAL_PI/main.c
+++ experiment/CAL_PI/main.c
@@ -8,25 +8,25 @@
 #include <monitor.h>
 #include <pthread.h>
 
-const int MAX_ITER=1000;
+const int MAX_ITER=1e5;
 pthread_mutex_t mutex;
-unsigned long in_cir = 0, total = 0;
+unsigned in_cir = 0, total = 0;
 
-int is_in_circle(double x, double y){
-    return 1.0 - x*x - y*y>=0;
+int is_in_circle(double x, double y) {
+    return 1.0 - x * x - y * y >= 0;
 }
 
-double get_val(){
-    double val = rand()%1000/1000.0;
-    return rand()&1?-val:val;
+double get_val() {
+    double val = rand() / (RAND_MAX * 1.0);
+    return rand() & 1 ? -val : val;
 }
 
-void *build_montecario(void*args){
-    for(int i =0;i<MAX_ITER;++i){
-        double x=get_val(), y=get_val();
+void *build_montecario(void*args) {
+    for (int i = 0; i < MAX_ITER; ++i) {
+        double x = get_val(), y = get_val();
         pthread_mutex_lock(&mutex);
         ++total;
-        in_cir+=is_in_circle(x,y)?1:0;
+        in_cir += is_in_circle(x, y) ? 1 : 0;
         pthread_mutex_unlock(&mutex);
     }
     return NULL;
@@ -34,15 +34,12 @@ void *build_montecario(void*args){
 
 int main(void) {
     srand(time(NULL));
-    for (int i = 0; i < 10; ++i) {
-        pthread_t pthread_id;
-        pthread_create(&pthread_id, NULL, build_montecario, NULL);
-        pthread_join(pthread_id, NULL);
-    }
+    pthread_mutex_init(&mutex,NULL);
+    pthread_t ls[10];
+    for (int i = 0; i < 10; ++i)pthread_create(ls + i, NULL, build_montecario, NULL);
+    for (int i = 0; i < 10; ++i)pthread_join(*(ls + i), NULL);
     sleep(1);
-    double PI = in_cir * 1.0 / total * 4;
-    printf("in cricle:%lu total:%lu PI = %lf\n", in_cir, total, PI);
-    FILE*f = fopen("result.txt","a");
-    fprintf(f, "%lf\n", PI);
+    double PI = 4.0 * in_cir / total;
+    printf("in cricle:%u total:%u PI = %lf\n", in_cir, total, PI);
     return 0;
 }
